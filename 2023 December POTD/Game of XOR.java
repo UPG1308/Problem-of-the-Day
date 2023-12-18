151ae65d1f528f103e52f5dbd4e1509663a34599
UDAYKUMAR P G <pguday4@gmail.com>
2023-12-18 22:35:24 +0530

    Create Game of XOR.java

---
+++ 2023 December POTD/Game of XOR.java
@@ -0,0 +1,9 @@
+class Solution {
+    static int gameOfXor(int N , int[] A) {
+        // code here
+        int ans = 0;
+        if(N%2 == 0)    return ans;
+        for(int i=0;i<N;i++)    ans ^= (i%2!=0?0:A[i]);
+        return ans;
+    }
+};
