984afb697132ed594412be0c91501db3ffd47eb6
UDAYKUMAR P G <pguday4@gmail.com>
2023-12-23 21:30:19 +0530

    Create Count More than nk Occurences.java

---
+++ 2023 December POTD/Count More than nk Occurences.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    //Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int n, int k) 
+    {
+        int count=0;
+        Arrays.sort(arr);
+        int threshold=n/k;
+
+        for (int i=0;i<n;i++) {
+            int currentCount = 1;
+            while(i<n-1 && arr[i]==arr[i+1]){
+                currentCount++;
+                i++;
+            }
+            if(currentCount>threshold){
+                count++;
+            }
+        }
+        return count;
+    }
+}
