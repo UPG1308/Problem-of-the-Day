162bbbec49b374f0f9ebfd0d51a85a08935c8584
UDAYKUMAR P G <pguday4@gmail.com>
2024-03-15 23:41:37 +0530

    Create Linked List that is Sorted Alternatingly.java

---
+++ 2024 March POTD/Linked List that is Sorted Alternatingly.java
@@ -0,0 +1,48 @@
+
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+   public Node sort(Node head){
+        //your code here, return the head of the sorted list
+        if(head == null || head.next == null)return head;
+        Node st = head;
+        Node temp = st.next;
+        while(temp != null){
+            if(temp.data >= st.data){
+                st = temp;
+                temp = temp.next;
+            }else{
+                st.next = temp.next;
+                Node prev = null;
+                Node curr = head;
+                while(curr != st && curr.data <= temp.data){
+                    prev = curr;
+                    curr = curr.next;
+                }
+                if(prev == null){
+                    temp.next = head;
+                    head = temp;
+                }else{
+                    temp.next = curr;
+                    prev.next = temp;
+                }
+            }
+            temp = st.next;
+        }
+        return head;
+   }
+
+
+}
