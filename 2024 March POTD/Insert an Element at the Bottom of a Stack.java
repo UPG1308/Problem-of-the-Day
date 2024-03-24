4e6a0a1e7fea24f323d4b517b616e251f45a012e
UDAYKUMAR P G <pguday4@gmail.com>
2024-03-24 19:53:21 +0530

    Create Insert an Element at the Bottom of a Stack.java

---
+++ 2024 March POTD/Insert an Element at the Bottom of a Stack.java
@@ -0,0 +1,16 @@
+class Solution {
+    public Stack<Integer> insertAtBottom(Stack<Integer> st, int x) {
+        insertAtBtm(st, x);
+        return st;
+    }
+    public void insertAtBtm(Stack<Integer> st, int x) {
+        if(st.isEmpty()){
+            st.push(x);
+            return;
+        }
+        int s= st.pop();
+        insertAtBtm(st,  x);
+        st.push(s);
+        return;
+    }
+}
