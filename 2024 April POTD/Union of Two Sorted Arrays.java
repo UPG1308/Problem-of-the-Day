f70647826336dc8a8a62ba41cc559df60a563162
UDAYKUMAR P G <pguday4@gmail.com>
2024-04-20 22:15:23 +0530

    Create Union of Two Sorted Arrays.java

---
+++ 2024 April POTD/Union of Two Sorted Arrays.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        SortedSet <Integer>set = new TreeSet <>();
+        for(int i :arr1){
+            set.add(i);
+        }
+        for(int i :arr2){
+            set.add(i);
+        }
+        ArrayList<Integer> l = new  ArrayList<Integer>();
+         for(int  i :set){
+           l.add(i);
+        }
+        return l;
+    }
+}
