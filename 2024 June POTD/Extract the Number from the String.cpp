7505e96d19b46859c88681b643c93822acd9a542
UDAYKUMAR P G <pguday4@gmail.com>
2024-06-22 22:41:45 +0530

    Create Extract the Number from the String.cpp

---
+++ 2024 June POTD/Extract the Number from the String.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    long long ExtractNumber(string sentence) {
+        // code here
+        stringstream ss(sentence);
+        string temp;
+        vector<string> v;
+        while(ss>>temp){
+          v.push_back(temp);  
+        }
+        vector<long long> res;
+        for(int i=0; i<v.size(); i++){
+            if(all_of(v[i].begin(), v[i].end(), ::isdigit)){
+                if(v[i].find('9')==string::npos) res.push_back(stoll(v[i]));
+            }
+        }
+        if(!res.size()) return -1;
+        else {
+            sort(res.begin(), res.end());
+            return res[res.size()-1];
+        }
+    }
+};
