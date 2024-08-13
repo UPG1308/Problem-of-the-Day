a4863d88ff4be82affdb13d17e0083ceb01c8bef
UDAYKUMAR P G <pguday4@gmail.com>
2024-08-13 21:33:42 +0530

    Create Square root of a number.cpp

---
+++ 2024 August POTD/Square root of a number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long floorSqrt(long n) {
+        long low = 1;
+        long high = n;
+        long ans = 0;
+        
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (mid * mid == n) {
+                return mid;
+            }
+            if (mid * mid < n) {
+                ans = mid;  
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
