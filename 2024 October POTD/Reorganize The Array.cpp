3787a78d36060ee14e1e27d5985dbb99cb01a2fd
UDAYKUMAR P G <pguday4@gmail.com>
2024-10-11 23:07:24 +0530

    Create Reorganize The Array.cpp

---
+++ 2024 October POTD/Reorganize The Array.cpp
@@ -0,0 +1,11 @@
+lass Solution {
+  public:
+    vector<int> rearrange(vector<int>& arr) {
+        // Code here
+        int n = arr.size();
+        for(int i = 0; i < n; ++i){
+            while(arr[i] != -1 and arr[i] != i) swap(arr[i], arr[arr[i]]);
+        }
+        return arr;
+    }
+};
