54847cca6c7a30277cfd92b61dc66db2a67e07fb
UDAYKUMAR P G <pguday4@gmail.com>
2025-04-29 21:18:17 +0530

    Create Sort a linked list of 0s, 1s and 2s.cpp

---
+++ 2025 April POTD/Sort a linked list of 0s, 1s and 2s.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    Node* segregate(Node* h) {
+        multiset<int> s;
+        Node *p = h;
+        
+        while(p) s.insert(p->data), p = p->next;
+        
+        p = h;
+        for(int i : s) p->data = i, p = p->next;
+        
+        return h;
+}
+};
