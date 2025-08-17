bdc9b9a565cb25b8e2864859cbcb41166fb42e74
UDAYKUMAR P G <pguday4@gmail.com>
2025-08-18 01:03:47 +0530

    Create Find H index.cpp

---
+++ 2025 August POTD/Find H index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& citations) {
+        // code here
+        int n = citations.size();
+        sort(citations.rbegin(), citations.rend());
+        int ans = 0;
+        for(int i = 0; i < n; ++i){
+          if(citations[i] >= i + 1) ans = i + 1;
+          else break;
+        }
+        return ans;
+    }
+};
