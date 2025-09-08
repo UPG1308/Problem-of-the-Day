15cda9de6f7046a8740114427c3189c01c71e8df
UDAYKUMAR P G <pguday4@gmail.com>
2025-09-08 20:38:37 +0530

    Create  Merge Sort for Linked List.cpp

---
+++ 2025 September POTD/Merge Sort for Linked List.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    Node* mergeSort(Node* head) {
+        // code here
+        priority_queue<int,vector<int>,greater<int>> pq;
+        Node* curr = head;
+        
+        while(curr) {
+            pq.push(curr->data);
+            curr = curr->next;
+        }
+        Node* ans = new Node(-1);
+        curr = ans;
+        
+        while(!pq.empty()) {
+            curr->next = new Node(pq.top());
+            pq.pop();
+            curr = curr->next;
+        }
+        
+        return ans->next;
+    }
+};
