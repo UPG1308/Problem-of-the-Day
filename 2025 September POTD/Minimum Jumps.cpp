b671c3e7bfcca39dadbf30f39fd4e64683567192
UDAYKUMAR P G <pguday4@gmail.com>
2025-09-11 18:28:05 +0530

    Create  Minimum Jumps.cpp

---
+++ 2025 September POTD/Minimum Jumps.cpp
@@ -0,0 +1,24 @@
+
+// Function to return minimum number of jumps to end of array
+
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        // Your code here  
+        int n = arr.size(), lastInd = 0, jumps = 0, maxRange = 0;
+        for(int i = 0;  i < n; ++i){
+            maxRange = max(maxRange, i + arr[i]);
+            if(i == lastInd){
+                if(maxRange == i){
+                    jumps = -1;
+                    break;
+                }
+                lastInd = maxRange;
+                jumps++;
+                if(lastInd >= n - 1)break;
+            }
+        }
+        return jumps;
+    }
+};
+
