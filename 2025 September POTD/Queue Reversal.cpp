19723a3c767ec1bf7d08fc8d5d6340d374a32528
UDAYKUMAR P G <pguday4@gmail.com>
2025-09-23 20:41:44 +0530

    Create  Queue Reversal.cpp

---
+++ 2025 September POTD/Queue Reversal.cpp
@@ -0,0 +1,19 @@
+#include <queue>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    void reverseQueue(queue<int> &q) {
+        stack<int> st;
+        while (!q.empty()) {
+            st.push(q.front());
+            q.pop();
+        }
+        while (!st.empty()) {
+            q.push(st.top());
+            st.pop();
+        }
+    }
+};
+
