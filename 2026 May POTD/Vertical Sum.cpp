8e0ce4e2c9ca8a47e9a72c7d8675219f4d53c8c7
UDAYKUMAR P G <pguday4@gmail.com>
2026-05-28 21:33:22 +0530

    Create  Vertical Sum.cpp

---
+++ 2026 May POTD/Vertical Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+  void dfs(Node* root,int key,map<int,int> &mp){
+      if(!root)return ;
+      mp[key]+=root->data;
+      dfs(root->left,key-1,mp);
+      dfs(root->right,key+1,mp);
+  }
+    vector<int> verticalSum(Node* root) {
+        // code here
+        vector<int> ans;
+        map<int,int> mp;
+        dfs(root,0,mp);
+        for(auto i:mp){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
